70f2a619298e1f90d0a5ba259df5ec1797a3f8ee
unknown <jensennics.dlrs@gmail.com>
2022-12-15 01:26:59 +0800

    Implemented for loop to print all the elements. Also tried if the length will be zero once all the elements were used in the loop.

--- fifo-queue.py
+++ fifo-queue.py
@@ -3,6 +3,11 @@ from queues import Queue
 fifo = Queue("1st", "2nd", "3rd")
 print(len(fifo))
 
+for element in fifo:
+    print(element)
+
+print(len(fifo))
+
 #fifo.enqueue("1st")
 #fifo.enqueue("2nd")
 #fifo.enqueue("3rd")
