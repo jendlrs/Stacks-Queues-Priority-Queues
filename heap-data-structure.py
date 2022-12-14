69764187614de3c527e371aee4c7a2a38bd879a4
unknown <jensennics.dlrs@gmail.com>
2022-12-15 02:14:12 +0800

    I added comments

--- heap-data-structure.py
+++ heap-data-structure.py
@@ -2,9 +2,14 @@ from heapq import heappush
 from heapq import heappop
 
 fruits = []
+
+#Adding Elements
 heappush(fruits, "orange")
 heappush(fruits, "apple")
 heappush(fruits, "banana")
 
+#remove elements
+heappop(fruits)
 
+#display the remaining elements
 print(fruits)
