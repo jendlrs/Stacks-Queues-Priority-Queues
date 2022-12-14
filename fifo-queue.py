0562fc0d0d059b03d576353834c54290d0712f76
unknown <jensennics.dlrs@gmail.com>
2022-12-15 01:21:13 +0800

    I made the class Queue iterable to be able to use len and accept initial elements.

--- fifo-queue.py
+++ fifo-queue.py
@@ -1,17 +1,15 @@
 from queues import Queue
 
-fifo = Queue()
+fifo = Queue("1st", "2nd", "3rd")
+print(len(fifo))
 
-fifo.enqueue("1st")
-fifo.enqueue("2nd")
-fifo.enqueue("3rd")
+#fifo.enqueue("1st")
+#fifo.enqueue("2nd")
+#fifo.enqueue("3rd")
 
-fifo.dequeue()
-'1st'
-fifo.dequeue()
-'2nd'
-fifo.dequeue()
-'3rd'
-
-
-
+#fifo.dequeue()
+#'1st'
+#fifo.dequeue()
+#'2nd'
+#fifo.dequeue()
+#'3rd'
