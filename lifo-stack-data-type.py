d206c0a9c939e4666909cb69cdaa55da6f8b66fc
unknown <jensennics.dlrs@gmail.com>
2022-12-15 01:55:50 +0800

    Implemented way of adding elements one by one and also used len to verify the number of elements.

--- lifo-stack-data-type.py
+++ lifo-stack-data-type.py
@@ -1,5 +1,13 @@
 from queues import Stack
 
-lifo = Stack("1st", "2nd", "3rd")
-for element in lifo:
-    print(element)
+#lifo = Stack("1st", "2nd", "3rd")
+#for element in lifo:
+ #   print(element)
+
+lifo = []
+
+lifo.append("1st")
+lifo.append("2nd")
+lifo.append("3rd")
+
+print(len(lifo))
