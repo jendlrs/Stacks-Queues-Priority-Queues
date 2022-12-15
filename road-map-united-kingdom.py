f0836a56306f1093d08b65a1d5b918645407a77b
unknown <jensennics.dlrs@gmail.com>
2022-12-15 19:30:22 +0800

    Using the class and function imported from graph.py, all functions are now working and it can now be displayed without any error.

--- road-map-united-kingdom.py
+++ road-map-united-kingdom.py
@@ -1,4 +1,10 @@
 import networkx as nx
+from graph import City, load_graph
 
 graph = nx.nx_agraph.read_dot("roadmap.dot")
-print(graph.nodes["london"])
+nodes, graph = load_graph("roadmap.dot", City.from_dict)
+
+print(nodes["london"])
+
+print(graph)
+#print(graph.nodes["london"])
