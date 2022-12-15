727c87bf84b0fb18e4f022cadc47f2b5f9f5ca83
unknown <jensennics.dlrs@gmail.com>
2022-12-15 23:47:30 +0800

    Added another function to be able to find any place in the country that has been granted in twentieth century. The breadth-first search is implemented.

--- road-map-united-kingdom.py
+++ road-map-united-kingdom.py
@@ -1,23 +1,36 @@
 import networkx as nx
 from graph import City, load_graph
 
-graph = nx.nx_agraph.read_dot("roadmap.dot")
+#graph = nx.nx_agraph.read_dot("roadmap.dot")
+#nodes, graph = load_graph("roadmap.dot", City.from_dict)
+
+#print(nodes["london"])
+#print(graph)
+
+#for neighbor in graph.neighbors(nodes["london"]):
+ #   print(neighbor.name)
+
+#for neighbor, weights in graph[nodes["london"]].items():
+ #   print(weights["distance"], neighbor.name)
+
+#def sort_by(neighbors, strategy):
+ #   return sorted(neighbors.items(), key=lambda item: strategy(item[1]))
+
+#def by_distance(weights):
+    #return float(weights["distance"])
+
+#for neighbor, weights in sort_by(graph[nodes["london"]], by_distance):
+ #   print(f"{weights['distance']:>3} miles, {neighbor.name}")
+
+def is_twentieth_century(year):
+   return year and 1901 <= year <= 2000
+
 nodes, graph = load_graph("roadmap.dot", City.from_dict)
+for node in nx.bfs_tree(graph, nodes["edinburgh"]):
+    print("📍", node.name)
+    if is_twentieth_century(node.year):
+        print("Found:", node.name, node.year)
+        break
+else:
+    print("Not found")
 
-print(nodes["london"])
-print(graph)
-
-for neighbor in graph.neighbors(nodes["london"]):
-    print(neighbor.name)
-
-for neighbor, weights in graph[nodes["london"]].items():
-    print(weights["distance"], neighbor.name)
-
-def sort_by(neighbors, strategy):
-    return sorted(neighbors.items(), key=lambda item: strategy(item[1]))
-
-def by_distance(weights):
-    return float(weights["distance"])
-
-for neighbor, weights in sort_by(graph[nodes["london"]], by_distance):
-    print(f"{weights['distance']:>3} miles, {neighbor.name}")
