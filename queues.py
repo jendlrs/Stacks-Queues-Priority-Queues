7182c8c4eae70f35357d6dfaf9e86a67519389e8
unknown <jensennics.dlrs@gmail.com>
2022-12-15 15:19:09 +0800

    I tried the updated program of queues in priority-queue.py, and the results now is sorted on the same priority. The earlier element that was pushed now gets the precedence.

--- queues.py
+++ queues.py
@@ -26,11 +26,12 @@ class Stack(Queue):
 class PriorityQueue:
     def __init__(self):
         self._elements = []
-        self._counter = count()
+        self._counter = count() #This will retain its current state in a tuple pushed onto the heap.
+                                #If the elements has same priotity, then the earlier that was pushed gets the preccedence.
 
     def enqueue_with_priority(self, priority, value):
         element = (-priority, next(self._counter), value)
         heappush(self._elements, element)
 
     def dequeue(self):
-        return heappop(self._elements)[-1]
+        return heappop(self._elements)[-1] #Used negative to properly access the last element
