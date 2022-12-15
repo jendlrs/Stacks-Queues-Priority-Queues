f1717df90a911d70ee6e3360d4aa80abb0e4edde
unknown <jensennics.dlrs@gmail.com>
2022-12-15 18:46:53 +0800

    Added classmethod and defined a function that takes a dictionary attributes from DOT file.

--- graph.py
+++ graph.py
@@ -6,3 +6,13 @@ class City(NamedTuple):
     year: int | None
     latitude: float
     longitude: float
+
+    @classmethod
+    def from_dict(cls, attrs):
+        return cls(
+            name=attrs["xlabel"],
+            country=attrs["country"],
+            year=int(attrs["year"]) or None,
+            latitude=float(attrs["latitude"]),
+            longitude=float(attrs["longitude"]),
+        )
