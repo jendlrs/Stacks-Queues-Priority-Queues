acbade8f8112d48cd486a91c9bfb7c35a8740564
unknown <jensennics.dlrs@gmail.com>
2022-12-15 23:55:45 +0800

    Added another function (order) that wraps the list of sorted neighbors. This results to other answer sue to the latitude of the places.

--- road-map-united-kingdom.py
+++ road-map-united-kingdom.py
@@ -25,8 +25,13 @@ from graph import City, load_graph
 def is_twentieth_century(year):
    return year and 1901 <= year <= 2000
 
+def order(neighbors):
+    def by_latitude(city):
+        return city.latitude
+    return iter(sorted(neighbors, key=by_latitude, reverse=True))
+    
 nodes, graph = load_graph("roadmap.dot", City.from_dict)
-for node in nx.bfs_tree(graph, nodes["edinburgh"]):
+for node in nx.bfs_tree(graph, nodes["edinburgh"], sort_neighbors=order):
     print("📍", node.name)
     if is_twentieth_century(node.year):
         print("Found:", node.name, node.year)
