ffa27f56682bc96a0df50dc6ffadd01d8cbee6ba
unknown <jensennics.dlrs@gmail.com>
2022-12-15 15:10:40 +0800

    Imported intertools module to count from 0 to infinity in concise way.

--- queues.py
+++ queues.py
@@ -1,5 +1,6 @@
 from collections import deque
 from heapq import heappop, heappush
+from itertools import count
 
 class Queue:
     def __init__(self, *elements):
@@ -25,9 +26,11 @@ class Stack(Queue):
 class PriorityQueue:
     def __init__(self):
         self._elements = []
+        self._counter = count()
 
     def enqueue_with_priority(self, priority, value):
-        heappush(self._elements, (-priority, value))
+        element = (-priority, next(self._counter), value)
+        heappush(self._elements, element)
 
     def dequeue(self):
-        return heappop(self._elements)[1]
+        return heappop(self._elements)[-1]
