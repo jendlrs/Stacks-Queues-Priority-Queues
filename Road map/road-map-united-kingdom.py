888eee27339802154a37d68a60732dae60ed2061
unknown <jensennics.dlrs@gmail.com>
2022-12-17 01:14:20 +0800

    Tested two for loops using networkx implementation and dijkstra's algorithm. Both functions yielded same output of shortest path.

--- Road map/road-map-united-kingdom.py
+++ Road map/road-map-united-kingdom.py
@@ -13,6 +13,8 @@ from graph import (
     depth_first_search as dfs,
 )
 
+from graph import City, load_graph, dijkstra_shortest_path
+
 #graph = nx.nx_agraph.read_dot("roadmap.dot")
 #nodes, graph = load_graph("roadmap.dot", City.from_dict)
 
@@ -69,7 +71,7 @@ from graph import (
  #   print(city.name)
 
 #Enumerate the shortest paths between two cities.
-nodes, graph = load_graph("roadmap.dot", City.from_dict)
+#nodes, graph = load_graph("roadmap.dot", City.from_dict)
 
 #city1 = nodes["aberdeen"]
 #city2 = nodes["perth"]
@@ -110,14 +112,33 @@ nodes, graph = load_graph("roadmap.dot", City.from_dict)
  #   print("Not found")
 
 #Order of Cities now follows a linear path
-def is_twentieth_century(city):
-    return city.year and 1901 <= city.year <= 2000
+#def is_twentieth_century(city):
+ #   return city.year and 1901 <= city.year <= 2000
 
-nodes, graph = load_graph("roadmap.dot", City.from_dict)
-city = dfs(graph, nodes["edinburgh"], is_twentieth_century)
-print(city.name)
+#nodes, graph = load_graph("roadmap.dot", City.from_dict)
+#city = dfs(graph, nodes["edinburgh"], is_twentieth_century)
+#print(city.name)
 
 #For loop to iterate the neighboring cities using depth first traverse function
-print()
-for city in depth_first_traverse(graph, nodes["edinburgh"]):
+#print()
+#for city in depth_first_traverse(graph, nodes["edinburgh"]):
+ #   print(city.name)
+
+#Dijkstra-shortest path
+
+nodes, graph = load_graph("roadmap.dot", City.from_dict)
+
+city1 = nodes["london"]
+city2 = nodes["edinburgh"]
+
+def distance(weights):
+    return float(weights["distance"])
+
+for city in dijkstra_shortest_path(graph, city1, city2, distance):
+    print(city.name)
+
+def weight(node1, node2, weights):
+    return distance(weights)
+
+for city in nx.dijkstra_path(graph, city1, city2, weight):
     print(city.name)
