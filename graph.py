ffe1dc8bdc638b0daf8c596cfd7fbcd91b3e472f
unknown <jensennics.dlrs@gmail.com>
2022-12-16 17:00:22 +0800

    Implemented a solution for sorting the neighbors. Now, the program can find the shortest parh between two nodes.

--- graph.py
+++ graph.py
@@ -29,17 +29,20 @@ def load_graph(filename, node_factory):
         for name1, name2, weights in graph.edges(data=True)
     )
 
-def breadth_first_traverse(graph, source):
+def breadth_first_traverse(graph, source, order_by=None):
     queue = Queue(source)
     visited = {source}
     while queue:
         yield (node := queue.dequeue())
-        for neighbor in graph.neighbors(node):
+        neighbors = list(graph.neighbors(node))
+        if order_by:
+            neighbors.sort(key=order_by)
+        for neighbor in neighbors:
             if neighbor not in visited:
                 visited.add(neighbor)
                 queue.enqueue(neighbor)
 
-def breadth_first_search(graph, source, predicate):
-    for node in breadth_first_traverse(graph, source):
+def breadth_first_search(graph, source, predicate, order_by=None):
+    for node in breadth_first_traverse(graph, source, order_by):
         if predicate(node):
             return node
