ba4defd2e5a673d0c83b4cb705d8af17eafe90ce
unknown <jensennics.dlrs@gmail.com>
2022-12-17 03:36:44 +0800

    Added class that sets the priority values and added the prioritized products with their corresponding priority values,

--- thread_safe_queues.py
+++ thread_safe_queues.py
@@ -30,6 +30,17 @@ class Product:
     def __str__(self):
         return self.label
 
+class Priority(IntEnum):
+    HIGH = 1
+    MEDIUM = 2
+    LOW = 3
+
+PRIORITIZED_PRODUCTS = (
+    Product(Priority.HIGH, ":1st_place_medal:"),
+    Product(Priority.MEDIUM, ":2nd_place_medal:"),
+    Product(Priority.LOW, ":3rd_place_medal:"),
+)
+
 PRODUCTS = (
     ":balloon:",
     ":cookie:",
